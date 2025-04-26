81ed2b34ba6f9f3039291399cf9a6c699dc33d83
M Boota <48630668+MSaqib123@users.noreply.github.com>
2025-04-26 18:12:58 +0500

    [Adding payment fixis and other ting in sales]

---
+++ MustafaviManagementApp/Components/PaymentDetailsViewComponent.cs
@@ -0,0 +1,19 @@
+﻿// /ViewComponents/SaleDetailsViewComponent.cs
+using MedicineStore.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+public class PaymentDetailsViewComponent : ViewComponent
+{
+    private readonly AppDbContext _db;
+    public PaymentDetailsViewComponent(AppDbContext db) => _db = db;
+
+    public async Task<IViewComponentResult> InvokeAsync(int id)
+    {
+        var details = await _db.PaymentDetails
+            .Where(d => d.PaymentId == id)
+            .ToListAsync();
+
+        return View(details);
+    }
+}
---
+++ MustafaviManagementApp/Components/SaleDetailsViewComponent.cs
@@ -0,0 +1,20 @@
+﻿// /ViewComponents/SaleDetailsViewComponent.cs
+using MedicineStore.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+public class SaleDetailsViewComponent : ViewComponent
+{
+    private readonly AppDbContext _db;
+    public SaleDetailsViewComponent(AppDbContext db) => _db = db;
+
+    public async Task<IViewComponentResult> InvokeAsync(int saleId)
+    {
+        var details = await _db.SaleDetails
+            .Where(d => d.SaleId == saleId)
+            .Include(d => d.Medicine)
+            .ToListAsync();
+
+        return View(details);
+    }
+}
